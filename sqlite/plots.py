4c0c6a02484734e7292214a06d1620bb0805a379
kasia-latkowska <kasia.latkowska15@gmail.com>
2021-03-11 21:22:05 -0500

    tkinter with sqlite and api tutorial

---
+++ sqlite/plots.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+root = Tk()
+root.title('Graph')
+root.geometry("400x200")
+
+def graph():
+    house_prices = np.random.normal(200000, 25000, 5000)
+    plt.hist(house_prices, 50)
+    plt.show()
+
+my_button = Button(root, text="Graph it", command=graph)
+my_button.pack()
+
+root.mainloop()
